8feca086afd1f6db7d13b23a9f5fd19a8052d62d
MingXuanHe <heminxuaniam@163.com>
2016-04-20 16:09:53 +0800

    The Demo of how to use this category

---
+++ MXCategoryDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MXCategoryDemo
+//
+//  Created by hemingxuan on 16/2/21.
+//  Copyright © 2016年 hemingxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MXCategoryDemo/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  MXCategoryDemo
+//
+//  Created by hemingxuan on 16/2/21.
+//  Copyright © 2016年 hemingxuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
